77dc6dd1734e7aeef8a92815cd57b7808c9e693d
Luke <freeman.luke@gmail.com>
2016-01-10 15:25:31 -0800

    path variable sustitution

--- router.go
+++ router.go
@@ -2,6 +2,7 @@ package goro
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -11,7 +12,7 @@ type route struct {
 	PathFormat   string
 	HasWildcards bool
 	Wildcards    []Match
-	Handler      http.Handler
+	Handler      http.HandlerFunc
 }
 
 type Router struct {
@@ -31,15 +32,15 @@ type Router struct {
 	ShouldRedirectTrailingSlash bool
 
 	// NotFoundHandler - route / resource not found handler
-	NotFoundHandler http.Handler
+	NotFoundHandler http.HandlerFunc
 
 	// MethodNotAllowedHandler - if defined, will be hit wen requesting a defined route
 	// via a non-defined http method (e.g.: requesting via POST when only GET is defined).
 	// if not defined, we will fallback to the NotFoundHandler
-	MethodNotAllowedHandler http.Handler
+	MethodNotAllowedHandler http.HandlerFunc
 
 	// PanicHandler - handler for when things gets real
-	PanicHandler http.Handler
+	PanicHandler http.HandlerFunc
 }
 
 func NewRouter() Router {
@@ -54,36 +55,43 @@ func NewRouter() Router {
 
 // variable registration
 func (r *Router) AddStringVar(variable string, value string) {
-	r.variables[variable] = value
+	wrappedVarName := value
+	if strings.HasPrefix(variable, "$") {
+		wrappedVarName = "{" + variable + "}"
+	} else if !strings.HasPrefix(variable, "{$") {
+		wrappedVarName = "{$" + variable + "}"
+	}
+	fmt.Printf("wrapped var = %s\n", wrappedVarName)
+	r.variables[wrappedVarName] = value
 }
 
 // route registration
 // DELETE - Convenience func for a call using the http DELETE method
-func (r *Router) DELETE(path string, handler http.Handler) {
+func (r *Router) DELETE(path string, handler http.HandlerFunc) {
 	r.Route("DELETE", path, handler)
 }
 
 // GET - Convenience func for a call using the http GET method
-func (r *Router) GET(path string, handler http.Handler) {
+func (r *Router) GET(path string, handler http.HandlerFunc) {
 	r.Route("GET", path, handler)
 }
 
 // PATCH - Convenience func for a call using the http PATCH method
-func (r *Router) PATCH(path string, handler http.Handler) {
+func (r *Router) PATCH(path string, handler http.HandlerFunc) {
 	r.Route("PATCH", path, handler)
 }
 
 // POST - Convenience func for a call using the http POST method
-func (r *Router) POST(path string, handler http.Handler) {
+func (r *Router) POST(path string, handler http.HandlerFunc) {
 	r.Route("POST", path, handler)
 }
 
 // PUT - Convenience func for a call using the http PUT method
-func (r *Router) PUT(path string, handler http.Handler) {
+func (r *Router) PUT(path string, handler http.HandlerFunc) {
 	r.Route("PUT", path, handler)
 }
 
-func (r *Router) Route(method string, path string, handler http.Handler) {
+func (r *Router) Route(method string, path string, handler http.HandlerFunc) {
 
 	routePath := path
 	wildcards, variables, wcErr := findWildcards(path)
@@ -161,4 +169,13 @@ func findWildcards(path string) (wildcards []Match, variables []Match, parseErr 
 		match = matcher.NextMatch()
 	}
 	return wildcardMatches, variableMatches, nil
+}
+
+func (r *Router) PrintRoutes() {
+	for k, routes := range r.registedRoutes {
+		fmt.Printf("%s:\n", k)
+		for _, match := range routes {
+			fmt.Printf("  - %s\n", match.PathFormat)
+		}
+	}
 }
--- router_test.go
+++ router_test.go
@@ -2,13 +2,35 @@ package goro
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 )
+
+type mockResponseWriter struct{}
+
+func (m *mockResponseWriter) Header() (h http.Header) {
+	return http.Header{}
+}
+
+func (m *mockResponseWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func (m *mockResponseWriter) WriteString(s string) (n int, err error) {
+	return len(s), nil
+}
+
+func (m *mockResponseWriter) WriteHeader(int) {}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HERE")
+}
 
 func TestRouter(t *testing.T) {
 
 	router := NewRouter()
-	router.AddStringVar("id_format", "id:[a-Z]+")
+	router.AddStringVar("id_format", "{id:[a-Z]+}")
+	router.AddStringVar("operation", "this_op")
 
 	paths := []string{
 		"hello",
@@ -17,13 +39,19 @@ func TestRouter(t *testing.T) {
 		"/{something}",
 		"/users/{$id_format}",
 		"/users/{first}.{second}",
-		"/users/{id}/{$operation}",
+		"/users/{$id_format}/{$operation}",
 		"/test/this/thing/{blah:[A-Z]+}",
 	}
 
 	for _, path := range paths {
-		router.GET(path, nil)
+		router.GET(path, testHandler)
 	}
-	fmt.Printf("registered routes:\n%v", router.registedRoutes)
+
+	router.PrintRoutes()
+
+	w := new(mockResponseWriter)
+	req, _ := http.NewRequest("GET", "/users/1234", nil)
+	router.ServeHTTP(w, req)
+
 	fmt.Printf("\n")
 }
