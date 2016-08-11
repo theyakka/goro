e98cf816c3dd45dcf1e10781fd9bfa4cb3638208
Luke <freeman.luke@gmail.com>
2016-08-11 15:56:03 -0700

    a lot of chainer modifications

--- chain.go
+++ chain.go
@@ -9,33 +9,62 @@
 
 package goro
 
-import (
-	"net/http"
-)
+import "net/http"
 
 // Chain allows for chaining of Handlers
 type Chain struct {
 	// Handlers - the handlers in the Chain
-	Handlers []http.Handler
+	Handlers       []ChainHandler
+	handlerIndex   int
+	responseWriter http.ResponseWriter
+	request        *http.Request
+}
+
+// ChainHandler - Handler wrapper with access to the chain
+type ChainHandler interface {
+	Execute(chain *Chain, w http.ResponseWriter, req *http.Request)
+}
+
+// ChainHandlerFunc - HandlerFunc wrapper with access to the chain
+type ChainHandlerFunc func(chain *Chain, w http.ResponseWriter, req *http.Request)
+
+func (chf ChainHandlerFunc) Execute(chain *Chain, w http.ResponseWriter, req *http.Request) {
+	chf(chain, w, req)
+}
+
+// chainHandlerWrapper - wraps a regular http.Handler for chaining purposes
+type chainHandlerWrapper struct {
+	handler http.Handler
+}
+
+// newChainHandlerWrapper - creates a new chain handler wrapper
+func newChainHandlerWrapper(handler http.Handler) chainHandlerWrapper {
+	return chainHandlerWrapper{
+		handler: handler,
+	}
+}
+
+func (chw chainHandlerWrapper) Execute(chain *Chain, w http.ResponseWriter, req *http.Request) {
+	chw.handler.ServeHTTP(w, req)
 }
 
 // NewChain creates a new Chain instance
-func NewChain() Chain {
-	return Chain{
-		Handlers: []http.Handler{},
+func NewChain(handlers ...ChainHandler) *Chain {
+	return &Chain{
+		Handlers: handlers,
 	}
 }
 
 // Add adds one or more Handlers to the end of the chain
-func (ch *Chain) Add(v ...http.Handler) *Chain {
+func (ch *Chain) Add(v ...ChainHandler) *Chain {
 	ch.Handlers = append(ch.Handlers, v...)
 	return ch
 }
 
 // AddFunc adds one or more HandlerFuncs to the end of the chain
-func (ch *Chain) AddFunc(v ...http.HandlerFunc) *Chain {
+func (ch *Chain) AddFunc(v ...ChainHandlerFunc) *Chain {
 	for _, hfunc := range v {
-		ch.Handlers = append(ch.Handlers, http.Handler(hfunc))
+		ch.Handlers = append(ch.Handlers, ChainHandler(hfunc))
 	}
 	return ch
 }
@@ -45,7 +74,8 @@ func (ch *Chain) Then(handler http.Handler) *Chain {
 	chain := &Chain{
 		Handlers: ch.Handlers,
 	}
-	chain.Add(handler)
+	wrapper := newChainHandlerWrapper(handler)
+	chain.Add(wrapper)
 	return chain
 }
 
@@ -54,8 +84,24 @@ func (ch *Chain) ThenFunc(handlerFunc http.HandlerFunc) *Chain {
 	return ch.Then(http.Handler(handlerFunc))
 }
 
+// Next - execute the next handler in the chain
+func (ch *Chain) Next() {
+	ch.handlerIndex += 1
+	if ch.handlerIndex < len(ch.Handlers) {
+		ch.Handlers[ch.handlerIndex].Execute(ch, ch.responseWriter, ch.request)
+	}
+}
+
+// Halt - halt chain execution
+func (ch *Chain) Halt() {
+	ch.handlerIndex = 0
+}
+
+// ServeHTTP - execute default functionality
 func (ch *Chain) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	for _, handler := range ch.Handlers {
-		handler.ServeHTTP(w, req)
+	if len(ch.Handlers) > 0 {
+		ch.responseWriter = w
+		ch.request = req
+		ch.Handlers[0].Execute(ch, w, req)
 	}
 }
